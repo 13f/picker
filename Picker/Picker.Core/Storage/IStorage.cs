8741922c1a46bbcb27b978c064c6eec4f34a6830
Jerin <taurenjerin@hotmail.com>
2015-03-29 21:16:59 +0800

    update Storage.
    Signed-off-by: Jerin <taurenjerin@hotmail.com>

--- Picker/Picker.Core/Storage/IStorage.cs
+++ Picker/Picker.Core/Storage/IStorage.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace Picker.Core.Storage {
   public interface IStorage {
     void OpenDatabase( string connString );
 
-    Task<int> Douban_SaveBook( string url, string json );
+    Task<int> Douban_SaveBook( string url, JObject data, bool saveChanges );
+
+    Task<int> Douban_SaveBooks( Dictionary<string, JObject> data );
 
     Task<int> Douban_DeleteBook( string url );
 
-    Task<int> Douban_SaveMovie( string url, string json );
+    Task<int> Douban_SaveMovie( string url, JObject data, bool saveChanges );
 
     Task<int> Douban_DeleteMovie( string url );
 
--- Picker/Picker.Postgresql/StoreContext.cs
+++ Picker/Picker.Postgresql/StoreContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 using Npgsql;
 using Picker.Core.Storage;
 
@@ -22,18 +23,27 @@ namespace Picker.Postgresql {
 
     #region Interfaces
 
-    public async Task<int> Douban_SaveBook( string url, string json ) {
+    public async Task<int> Douban_SaveBook( string url, JObject data, bool saveChanges ) {
       Book book = doubanContext.Book.Where( i => i.Uri == url ).FirstOrDefault();
       if ( book != null ) {
-        book.Content = json;
+        book.Content = data.ToString();
         book.UpdatedAt = DateTime.UtcNow;
       }
       else {
         book = new Book();
-        book.Content = json;
+        book.Content = data.ToString();
         book.CreatedAt = DateTime.UtcNow;
         book.UpdatedAt = DateTime.UtcNow;
         doubanContext.Book.Add( book );
+      }
+      if ( saveChanges )
+        return await doubanContext.SaveChangesAsync();
+      return 0;
+    }
+
+    public async Task<int> Douban_SaveBooks( Dictionary<string, JObject> data ) {
+      foreach ( var item in data ) {
+        await Douban_SaveBook( item.Key, item.Value, false );
       }
       return await doubanContext.SaveChangesAsync();
     }
@@ -47,7 +57,7 @@ namespace Picker.Postgresql {
       return 0;
     }
 
-    public async Task<int> Douban_SaveMovie( string url, string json ) {
+    public async Task<int> Douban_SaveMovie( string url, JObject data, bool saveChanges ) {
       //Book book = doubanContext.Book.Where( i => i.Uri == url ).FirstOrDefault();
       //if ( book != null ) {
       //  book.Content = json;
