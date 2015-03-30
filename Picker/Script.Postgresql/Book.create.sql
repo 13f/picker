6666b7b9e5ff51dc164705c510d66f2b7e433c8c
Jerin <taurenjerin@hotmail.com>
2015-03-30 11:43:49 +0800

    增加Book、BookTask的创建脚本。
    Signed-off-by: Jerin <taurenjerin@hotmail.com>

---
+++ Picker/Script.Postgresql/Book.create.sql
@@ -0,0 +1,18 @@
+﻿-- Table: "Book"
+
+-- DROP TABLE "Book";
+
+CREATE TABLE "Book"
+(
+  "Uri" character varying(255) NOT NULL,
+  "CreatedAt" timestamp(6) with time zone,
+  "UpdatedAt" timestamp(6) with time zone,
+  "ProcessedAt" timestamp(6) with time zone,
+  "Content" text,
+  CONSTRAINT "PrimaryKey_Book_Uri" PRIMARY KEY ("Uri")
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Book"
+  OWNER TO postgres;
---
+++ Picker/Script.Postgresql/BookTask.create.sql
@@ -0,0 +1,15 @@
+﻿-- Table: "BookTask"
+
+-- DROP TABLE "BookTask";
+
+CREATE TABLE "BookTask"
+(
+  "ApiUri" character varying(255) NOT NULL,
+  "ProcessedAt" timestamp(6) with time zone,
+  CONSTRAINT "PrimaryKey_BookTask_ApiUri" PRIMARY KEY ("ApiUri")
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "BookTask"
+  OWNER TO postgres;
