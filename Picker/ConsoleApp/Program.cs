7678a8a14de6a2c421639ac5ec7ee77fc1b0d747
Jerin <taurenjerin@hotmail.com>
2015-03-31 10:46:44 +0800

    增加有关PostgreSQL存储的测试代码。
    Signed-off-by: Jerin <taurenjerin@hotmail.com>

--- Picker/ConsoleApp/Program.cs
+++ Picker/ConsoleApp/Program.cs
@@ -4,33 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Picker.Core;
+using Picker.Core.Spider;
+using Picker.Core.Storage;
+using Picker.Postgresql;
 
 namespace ConsoleApp {
   class Program {
     static void Main( string[] args ) {
       // test1(); // 0k @ 2015-3-29
       // test2(); // 0k @ 2015-3-29
+      test3();
     }
 
-    /// <summary>
-    /// 测试JSON的用法；测试读取某人收藏的书籍
-    /// </summary>
-    static void test1() {
-      Picker.Core.Spider.DoubanApi douban = new Picker.Core.Spider.DoubanApi();
-      var task = douban.GetMyBookCollections( "taurenshaman", 0 );
-      Task.WaitAll( task );
-      Console.Write( task.Result );
-      Console.ReadKey();
-    }
+    ///// <summary>
+    ///// 测试JSON的用法；测试读取某人收藏的书籍
+    ///// </summary>
+    //static void test1() {
+    //  Picker.Core.Spider.DoubanApi douban = new Picker.Core.Spider.DoubanApi();
+    //  var task = douban.GetMyBookCollections( "taurenshaman", 0 );
+    //  Task.WaitAll( task );
+    //  Console.Write( task.Result );
+    //  Console.ReadKey();
+    //}
 
-    /// <summary>
-    /// 测试读取某个系列的书籍
-    /// </summary>
-    static void test2() {
-      Picker.Core.Spider.DoubanApi douban = new Picker.Core.Spider.DoubanApi();
-      var task = douban.GetBooksOfSerie( "1163", 0 );
+    ///// <summary>
+    ///// 测试读取某个系列的书籍
+    ///// </summary>
+    //static void test2() {
+    //  Picker.Core.Spider.DoubanApi douban = new Picker.Core.Spider.DoubanApi();
+    //  var task = douban.GetBooksOfSerie( "1163", 0 );
+    //  Task.WaitAll( task );
+    //  Console.Write( task.Result );
+    //  Console.ReadKey();
+    //}
+
+    static void test3() {
+      string executablePath = System.Reflection.Assembly.GetEntryAssembly().Location;
+      string executableFolder = System.IO.Path.GetDirectoryName( executablePath );
+      var config = new Configuration( executableFolder );
+
+      //var store = new StoreContext( "name=Douban" );
+      var store = new StoreContext( "metadata=res://*/DoubanModel.csdl|res://*/DoubanModel.ssdl|res://*/DoubanModel.msl;provider=Npgsql;provider connection string=&quot;PORT=5432;TIMEOUT=15;POOLING=True;MINPOOLSIZE=1;MAXPOOLSIZE=20;COMMANDTIMEOUT=20;COMPATIBLE=2.2.5.0;HOST=localhost;DATABASE=douban;USER ID=postgres;PASSWORD=whosyourdaddy&quot;" );
+      var api = new DoubanApi( "" );
+      var biz = new Douban( api, store, config );
+
+      var task = biz.StartUserTask( false );
       Task.WaitAll( task );
-      Console.Write( task.Result );
       Console.ReadKey();
     }
 
