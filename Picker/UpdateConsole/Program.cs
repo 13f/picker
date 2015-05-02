151d05ff83f59672366dee40af7e4eca1ad7eeb9
Jerin <taurenjerin@hotmail.com>
2015-05-02 23:10:43 +0800

    增加升级项目：补全User的type字段。
    Signed-off-by: Jerin <taurenjerin@hotmail.com>

---
+++ Picker/UpdateConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpdateConsole {
+  class Program {
+    static void Main( string[] args ) {
+      string conn = System.Configuration.ConfigurationManager.ConnectionStrings["Postgresql_Douban"].ConnectionString;
+      Update201505AddUserType.Run( conn );
+      Console.ReadKey();
+    }
+  }
+}
---
+++ Picker/UpdateConsole/Update201505AddUserType.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Picker.Postgresql;
+using Newtonsoft;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace UpdateConsole {
+  public static class Update201505AddUserType {
+    /// <summary>
+    /// 每次批处理n条提交一次
+    /// </summary>
+    const int batchCount = 20;
+    const string propertyName = "type";
+
+    public static void Run( string conn ) {
+      DoubanEntities db = new DoubanEntities( conn );
+      Console.WriteLine( "给User表增加type字段，以区分user、virtual或site。" );
+      process( db );
+      Console.WriteLine( "Finished...." );
+    }
+
+    static void process( DoubanEntities db ) {
+      int count = 0;
+      int processedCount = 0;
+      long total = db.User.LongCount();
+      do {
+        var data = db.User.Where( u => u.type == null || u.type.Length == 0 ).Take( batchCount );
+        foreach ( User user in data ) {
+          var obj = JObject.Parse( user.Content );
+          if ( obj == null )
+            continue;
+          user.type = (string)obj[propertyName];
+          processedCount++;
+        }
+        count = db.SaveChanges();
+        Console.WriteLine( "Done: " + processedCount.ToString() + " / " + total.ToString() );
+      }
+      while ( count >= batchCount );
+    }
+
+  }
+
+}
