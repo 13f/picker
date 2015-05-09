9ba2b0458e29049e3f57f17e838a7cba5d0aa439
Jerin <taurenjerin@hotmail.com>
2015-05-09 22:34:12 +0800

    add NetHelper.
    Signed-off-by: Jerin <taurenjerin@hotmail.com>

---
+++ Picker/Picker.Core/Helpers/NetHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Picker.Core.Helpers {
+  public static class NetHelper {
+    public static WebClient GetWebClient_UTF8(){
+      WebClient client = new WebClient();
+      client.Encoding = System.Text.Encoding.UTF8;
+      client.UseDefaultCredentials = true;
+      return client;
+    }
+
+    public static WebClient GetWebClient_GB2312() {
+      WebClient client = new WebClient();
+      client.Encoding = System.Text.Encoding.GetEncoding( "GB2312" );
+      client.UseDefaultCredentials = true;
+      return client;
+    }
+
+  }
+
+}
