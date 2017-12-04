4c6181303503820cc341987b34fde03cb54c1592
Jerin Lee <taurenjerin@qq.com>
2017-12-04 09:01:23 +0800

    更新nuget；增加LicenseData。
    Signed-off-by: Jerin Lee <taurenjerin@qq.com>

---
+++ Consoles/ConsoleDBApp/Biz/LicenseBiz.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Picker.Core.Extensions;
+using Picker.Core.Helpers;
+
+namespace ConsoleDBApp.Biz {
+  public class LicenseBiz {
+
+  }
+
+}
